ef9f1d1c3fa0f0329e5da3f66a1bfb5688746d64
Matěj Laitl <matej@laitl.cz>
2020-10-22 20:51:34 +0200

    Add canvas, console_error_panic_hook
    I have drawn a rectangle from WASM, yay.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,25 @@
-use log::{debug, error, info, trace, warn, Level};
-use wasm_bindgen::prelude::wasm_bindgen;
+use log::{info, Level};
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 
 #[wasm_bindgen(start)]
 pub fn start() {
-    console_log::init_with_level(Level::Debug).expect("Logging can be initialized");
-    trace!("trace");
-    debug!("debug");
-    info!("info");
-    warn!("warn");
-    error!("error");
-    let window = web_sys::window().expect("window object exists");
-    window
-        .alert_with_message(&format!("Hello, {}!", "Matejaku"))
-        .expect("call succeeds");
+    console_error_panic_hook::set_once();
+    console_log::init_with_level(Level::Debug).expect("logging can be initialized");
+
+    draw_on_canvas();
+    info!("WASM Rain init done.");
+}
+
+fn draw_on_canvas() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas: HtmlCanvasElement = document
+        .get_element_by_id("canvas")
+        .expect("#canvas element exists")
+        .dyn_into()
+        .expect("#canvas is a HTML <canvas> element");
+    let context: CanvasRenderingContext2d =
+        canvas.get_context("2d").unwrap().unwrap().dyn_into().expect("CanvasRenderingContext2d");
+
+    context.fill_rect(10.0, 20.0, 30.0, 40.0);
 }
