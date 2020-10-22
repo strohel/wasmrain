a6c79a46238f8fcea32fe83157f3603940c2e718
Matěj Laitl <matej@laitl.cz>
2020-10-22 18:11:34 +0200

    Use web-sys crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 use log::{debug, error, info, trace, warn, Level};
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
 #[wasm_bindgen(start)]
 pub fn start() {
     console_log::init_with_level(Level::Debug).expect("Logging can be initialized");
@@ -14,5 +9,8 @@ pub fn start() {
     info!("info");
     warn!("warn");
     error!("error");
-    alert(&format!("Hello, {}!", "Matejaku"));
+    let window = web_sys::window().expect("window object exists");
+    window
+        .alert_with_message(&format!("Hello, {}!", "Matejaku"))
+        .expect("call succeeds");
 }
