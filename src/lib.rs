6d92cd288d69e3aeba9056a3bdc369e74ab94aae
Matěj Laitl <matej@laitl.cz>
2020-10-24 01:21:57 +0200

    Hook into HTML elements, start being a bit interactive

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use log::{info, Level};
+use std::str::FromStr;
 use wasm_bindgen::{prelude::*, JsCast};
-use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
+use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, HtmlInputElement, Window};
 
 const BLOCK_PIXELS: f64 = 30.0;
 
@@ -12,17 +13,63 @@ const SKY_COLOR: &str = "#edf4f4";
 const CLOUD_COLOR: &str = "#b0b8bb";
 
 #[wasm_bindgen(start)]
-pub fn start() {
+pub fn initialize() {
     console_error_panic_hook::set_once();
     console_log::init_with_level(Level::Debug).expect("logging can be initialized");
 
-    draw_on_canvas(&[20.0, 18.0, 19.0, 16.0, 18.0, 14.0, 17.0, 12.0, 16.0, 10.0, 15.0]);
+    let start_button = document().get_element_by_id("start").expect("#start element exists");
+
+    let boxed_fn = Closure::wrap(Box::new(simulate_world) as Box<dyn Fn()>).into_js_value();
+    start_button
+        .add_event_listener_with_callback("click", boxed_fn.as_ref().unchecked_ref())
+        .expect("callback can be attached");
     info!("WASM Rain init done.");
 }
 
+fn document() -> Document {
+    window().document().unwrap()
+}
+
+fn window() -> Window {
+    web_sys::window().unwrap()
+}
+
+fn simulate_world() {
+    let doc = document();
+    let landscape_string: String = input_element(&doc, "landscape").value();
+    let landscape_result: Result<Vec<_>, _> = landscape_string
+        .split(&[' ', ','][..])
+        .map(|string| {
+            f64::from_str(string).map_err(|e| format!("Cannot parse '{}' as number: {}", string, e))
+        })
+        .collect();
+    let landscape = match landscape_result {
+        Ok(landscape) => landscape,
+        Err(error) => {
+            window().alert_with_message(&error).expect("alert() works");
+            return;
+        }
+    };
+
+    let rain_hours = input_element(&doc, "rain").value_as_number();
+
+    info!("Simulate world with landscape: {:?} and {} hours of rain.", landscape, rain_hours);
+    draw_on_canvas(&landscape);
+
+    let start_button = input_element(&doc, "start");
+    start_button.set_value("Raining...");
+    start_button.set_disabled(true);
+}
+
+fn input_element(doc: &Document, id: &str) -> HtmlInputElement {
+    doc.get_element_by_id(id)
+        .expect("element with given id exists")
+        .dyn_into()
+        .expect("element is html <input>")
+}
+
 fn draw_on_canvas(surface: &[f64]) {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas: HtmlCanvasElement = document
+    let canvas: HtmlCanvasElement = document()
         .get_element_by_id("canvas")
         .expect("#canvas element exists")
         .dyn_into()
